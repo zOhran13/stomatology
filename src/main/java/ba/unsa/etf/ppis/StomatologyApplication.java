f7088a0d9adf68810f6e3917154cec51f57c355f
Anida Nezovic <anezovic1@etf.unsa.ba>
2024-04-29 23:14:52 +0200

    added one more article

--- src/main/java/ba/unsa/etf/ppis/StomatologyApplication.java
+++ src/main/java/ba/unsa/etf/ppis/StomatologyApplication.java
@@ -38,7 +38,7 @@ public class StomatologyApplication implements CommandLineRunner {
 		Article article = new Article();
 		article.setId("aaaaaaaa");
 		article.setAuthor("9d0d4a57-8623-4686-9d2f-06c2640c78c8");
-		article.setTitle("Ovo je NWT projekat.");
+		article.setTitle("Ovo je PPIS projekat.");
 
 		Text textSection = new Text();
 		textSection.setContent("Ovo je tekst.");
@@ -63,31 +63,31 @@ public class StomatologyApplication implements CommandLineRunner {
 		imageRepo.save(imageSection);
 
 		// drugi clanak
-//		Article article2 = new Article();
-//		article2.setAuthor("0dbac29d-a95c-42cd-bc0d-6809cad9e4fb");
-//		article2.setTitle("Drugi clanak.");
-//
-//		Text textSection2 = new Text();
-//		textSection2.setContent("Jos jedan tekst.");
-//
-//		Video videoSection2 = new Video();
-//		videoSection2.setVideoUrl("Jos jedan video.");
-//
-//		Image imageSection2 = new Image();
-//		imageSection2.setImageUrl("Jos jedna slika.");
-//
-//		article2.setText(textSection2);
-//		article2.setImage(imageSection2);
-//		article2.setVideo(videoSection2);
-//
-//		articleRepo.save(article2);
-//
-//		textSection2.setArticle(article2);
-//		textRepo.save(textSection2);
-//		videoSection2.setArticle(article2);
-//		videoRepo.save(videoSection2);
-//		imageSection2.setArticle(article2);
-//		imageRepo.save(imageSection2);
+		Article article2 = new Article();
+		article2.setAuthor("0dbac29d-a95c-42cd-bc0d-6809cad9e4fb");
+		article2.setTitle("Drugi clanak.");
+
+		Text textSection2 = new Text();
+		textSection2.setContent("Jos jedan tekst.");
+
+		Video videoSection2 = new Video();
+		videoSection2.setVideoUrl("Jos jedan video.");
+
+		Image imageSection2 = new Image();
+		imageSection2.setImageUrl("Jos jedna slika.");
+
+		article2.setText(textSection2);
+		article2.setImage(imageSection2);
+		article2.setVideo(videoSection2);
+
+		articleRepo.save(article2);
+
+		textSection2.setArticle(article2);
+		textRepo.save(textSection2);
+		videoSection2.setArticle(article2);
+		videoRepo.save(videoSection2);
+		imageSection2.setArticle(article2);
+		imageRepo.save(imageSection2);
 
 	}
 
