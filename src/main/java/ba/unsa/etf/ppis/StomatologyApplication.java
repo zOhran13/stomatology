795473d6a3f703f3aa994ab813aa28f25f59b6c3
aabdagic2 <aabdagic2@etf.unsa.ba>
2024-05-19 18:10:52 +0200

    Implemented lecture management

--- src/main/java/ba/unsa/etf/ppis/StomatologyApplication.java
+++ src/main/java/ba/unsa/etf/ppis/StomatologyApplication.java
@@ -39,55 +39,43 @@ public class StomatologyApplication implements CommandLineRunner {
 		article.setId("aaaaaaaa");
 		article.setAuthor("9d0d4a57-8623-4686-9d2f-06c2640c78c8");
 		article.setTitle("Ovo je PPIS projekat.");
-
 		Text textSection = new Text();
 		textSection.setContent("Ovo je tekst.");
-
 		Video videoSection = new Video();
 		videoSection.setVideoUrl("Ovo je video.");
-
 		Image imageSection = new Image();
 		imageSection.setImageUrl("Ovo je slika.");
-
 		article.setText(textSection);
 		article.setImage(imageSection);
 		article.setVideo(videoSection);
-
 		articleRepo.save(article);
-
 		textSection.setArticle(article);
 		textRepo.save(textSection);
 		videoSection.setArticle(article);
 		videoRepo.save(videoSection);
 		imageSection.setArticle(article);
 		imageRepo.save(imageSection);
-*/
-		// drugi clanak
-		Article article2 = new Article();
+*/// drugi clanak
+	/*	Article article2 = new Article();
 		article2.setAuthor("0dbac29d-a95c-42cd-bc0d-6809cad9e4fb");
 		article2.setTitle("Drugi clanak.");
-
 		Text textSection2 = new Text();
 		textSection2.setContent("Jos jedan tekst.");
-
 		Video videoSection2 = new Video();
 		videoSection2.setVideoUrl("Jos jedan video.");
-
 		Image imageSection2 = new Image();
 		imageSection2.setImageUrl("Jos jedna slika.");
-
 		article2.setText(textSection2);
 		article2.setImage(imageSection2);
 		article2.setVideo(videoSection2);
-
 		articleRepo.save(article2);
-
 		textSection2.setArticle(article2);
 		textRepo.save(textSection2);
 		videoSection2.setArticle(article2);
 		videoRepo.save(videoSection2);
 		imageSection2.setArticle(article2);
-		imageRepo.save(imageSection2);
+		imageRepo.save(imageSection2);*/
+
 
 	}
 
